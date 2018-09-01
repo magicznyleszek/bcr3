196e23dfc06664ab6f734e483eec350c127544d8
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2018-09-01 09:37:37 +0200

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: {
+    "postcss-import": {},
+    "postcss-cssnext": {
+      browsers: "> 5%"
+    },
+    "postcss-custom-properties": {
+      preserve: false
+    },
+    "postcss-color-function": {},
+    cssnano: {
+      autoprefixer: false
+    }
+  }
+};
